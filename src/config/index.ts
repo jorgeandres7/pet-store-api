17baf27320f0f915acf854a145c3c71e37a5afdd
Steven Villegas <syivenvillegas0@gmail.com>
2024-06-20 15:21:43 -0500

    Implementacion de Hashing en contraseñas

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,8 @@
-// config/index.ts
-export * from './envs';
-export * from './validator';//1
+export * from "./envs";
+export * from "./validators";
+
+/* `export * from "./bcrypt";` está exportando todas las exportaciones nombradas desde el módulo
+"./bcrypt". Esto significa que cualquier exportación con nombre (funciones, variables, clases, etc.)
+definida en el módulo "./bcrypt" estará disponible para importar en cualquier módulo que importe
+este archivo. */
+export * from "./bcrypt";
