8a12bd89df8dd2ebc16568ebc2b5082aa9aa1e5a
Steven Villegas <syivenvillegas0@gmail.com>
2024-06-20 16:38:57 -0500

    Implementacion mapper para el objeto de entidades

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 export * from "./envs";
-export * from "./validators";
+export * from "./validator";
 
 /* `export * from "./bcrypt";` está exportando todas las exportaciones nombradas desde el módulo
 "./bcrypt". Esto significa que cualquier exportación con nombre (funciones, variables, clases, etc.)
 definida en el módulo "./bcrypt" estará disponible para importar en cualquier módulo que importe
 este archivo. */
-export * from "./bcrypt";
+export * from "./bcript";
